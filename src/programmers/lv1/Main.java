103bab680c34a450f3a5c728b4521d3b2073c8a8
Connor <kor.connor@gmail.com>
2021-09-22 19:39:45 +0900

    improve 가운데 글자 가져오기

--- src/programmers/lv1/Main.java
+++ src/programmers/lv1/Main.java
@@ -3,8 +3,12 @@ package programmers.lv1;
 public class Main {
 	public static void main(String[] args) {
 		가운데글자가져오기 s = new 가운데글자가져오기();
+//		int[] d = {1, 3, 2, 5, 4};
+//		int budget = 9;
+//		System.out.println(s.solution(d, budget));
 		String str = "qwer";
 		System.out.println(s.solution(str));
+		
 		
 	}
 }
--- src/programmers/lv1/가운데글자가져오기.java
+++ src/programmers/lv1/가운데글자가져오기.java
@@ -2,9 +2,7 @@ package programmers.lv1;
 
 public class 가운데글자가져오기 {
     public String solution(String s) {
-        int half = s.length() / 2;
-        
-        return s.length() % 2 == 0 ? s.substring(half - 1, half + 1) :
-        s.charAt(half) + "";
+        int len = s.length();
+        return s.substring((len-1) / 2, (len+2) / 2);
     }
 }
