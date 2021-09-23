b1338140bb8da06600d50656da573be98b3cf28c
Connor <kor.connor@gmail.com>
2021-09-24 00:59:51 +0900

    다트게임

--- src/programmers/lv1/Main.java
+++ src/programmers/lv1/Main.java
@@ -2,13 +2,12 @@ package programmers.lv1;
 
 public class Main {
 	public static void main(String[] args) {
-		가운데글자가져오기 s = new 가운데글자가져오기();
+		다트게임_1차 s = new 다트게임_1차();
 //		int[] d = {1, 3, 2, 5, 4};
 //		int budget = 9;
 //		System.out.println(s.solution(d, budget));
-		String str = "qwer";
+		String str = "1D2S#10S";
 		System.out.println(s.solution(str));
-		
 		
 	}
 }
--- src/programmers/lv1/다트게임_1차.java
+++ src/programmers/lv1/다트게임_1차.java
@@ -1,12 +1,28 @@
 package programmers.lv1;
 
+import java.util.Arrays;
+
 public class 다트게임_1차 {
     public int solution(String dartResult) {
-        int answer = 0;
+        int n = 0;
+        int[] scores = new int[3];
+        int idx = -1;
+        boolean isTen = false;
         
-        
-        
-        return answer;
+        for (char c : dartResult.toCharArray()) {
+        	if (Character.isDigit(c)) {
+        		n = isTen ? 10 : c - '0';
+        		if (!isTen) idx++;
+        		isTen = true;
+        	} else if (Character.isAlphabetic(c)) {
+        		scores[idx] = (int) Math.pow(n, c == 'S' ? 1 : c == 'D' ? 2 : 3);
+        		isTen = false;
+        	}
+        	else {
+        		scores[idx] *= c == '*' ? 2 : -1;
+        		if (c == '*' && idx != 0) scores[idx - 1] *= 2;
+        	}
+		}
+        return Arrays.stream(scores).sum();
     }
-
 }
