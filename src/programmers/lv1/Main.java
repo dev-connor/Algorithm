8edb73d70df9f2a871e1dfd3f7824dab39138564
Connor <kor.connor@gmail.com>
2021-09-24 22:26:31 +0900

    improve 두 정수 사이의 합

--- src/programmers/lv1/Main.java
+++ src/programmers/lv1/Main.java
@@ -2,10 +2,10 @@ package programmers.lv1;
 
 public class Main {
 	public static void main(String[] args) {
-		소수찾기 s = new 소수찾기();
+		두정수사이의합 s = new 두정수사이의합();
 		String str = "1234";
 		int n = 10;
-		System.out.println(s.solution(n));
+		System.out.println(s.solution(5,3));
 		
 	}
 }
