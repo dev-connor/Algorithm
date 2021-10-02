562a3b7be24c0b682f13bbeacd64d63d4a3c1b38
Connor <kor.connor@gmail.com>
2021-10-03 00:38:43 +0900

    소수만들기 파일명 변경

--- src/programmers/lv1/소수만들기memoization.java
+++ src/programmers/lv1/소수만들기memoization.java
@@ -1,14 +1,22 @@
 package programmers.lv1;
 
-class 소수만들기short {
+import java.util.HashMap;
+
+class 소수만들기memoization {
 	public int solution(int[] nums) {
+		HashMap<Integer, Boolean> primes = new HashMap<>();
 		int count = 0;
 
 		for (int i = 0; i < nums.length - 2; i++) {
 			for (int j = i + 1; j < nums.length - 1; j++) {
 				for (int k = j + 1; k < nums.length; k++) {
 					int sum = nums[i] + nums[j] + nums[k];
-					if (isPrime(sum)) count++;
+					
+					if (primes.containsKey(sum)) count += primes.get(sum) ? 1 : 0;
+					else if (isPrime(sum)) {
+						count++;
+						primes.put(sum, true);
+					} else primes.put(sum, false);
 				}
 			}
 		}
@@ -16,9 +24,8 @@ class 소수만들기short {
 	}
 
 	private boolean isPrime(int num) {
-		for (int i = 2; i <= Math.sqrt(num); i++) {
+		for (int i = 2; i <= Math.sqrt(num); i++) 
 			if (num % i == 0) return false;
-		}
 		return true;
 	}
 }
