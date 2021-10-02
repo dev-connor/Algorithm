6ac486a458ee8d79d650288f05c7b60325982f33
Connor <kor.connor@gmail.com>
2021-10-02 21:37:53 +0900

    improve 문자열 다루기 기본

--- src/programmers/lv1/Main.java
+++ src/programmers/lv1/Main.java
@@ -1,12 +1,10 @@
 package programmers.lv1;
 
-import java.util.Arrays;
-
 public class Main {
 	public static void main(String[] args) {
-		같은숫자는싫어 s = new 같은숫자는싫어();
-		int[] arr = {7, 1, 3, 3, 0, 1, 8};
-		System.out.println(Arrays.toString(s.solution(arr)));
+		문자열다루기기본 s = new 문자열다루기기본();
+		String str = "a234";
+		System.out.println(s.solution(str));
 		
 	}
 }
--- src/programmers/lv1/문자열다루기기본.java
+++ src/programmers/lv1/문자열다루기기본.java
@@ -2,6 +2,6 @@ package programmers.lv1;
 
 public class 문자열다루기기본 {
     public boolean solution(String s) {
-    	return s.matches("\\d{4}|\\d{6}") ? true : false;
+    	return s.matches("\\d{4}|\\d{6}");
     }
 }
