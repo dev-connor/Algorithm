3a125c871238fe83ebfdd6013a01b360a17f8a88
Connor <kor.connor@gmail.com>
2021-10-02 22:34:36 +0900

    improve 소수찾기

--- src/programmers/lv1/소수찾기.java
+++ src/programmers/lv1/소수찾기.java
@@ -4,16 +4,14 @@ public class 소수찾기 {
     public int solution(int n) {
         int count = 0;
         
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++)
 			if (isPrime(i)) count++;
-		}
         return count;
     }
 
 	private boolean isPrime(int num) {
-		for (int i = 2; i <= Math.sqrt(num); i++) {
+		for (int i = 2; i <= Math.sqrt(num); i++)
 			if (num % i == 0) return false;
-		}
 		return true;
 	}
 }
