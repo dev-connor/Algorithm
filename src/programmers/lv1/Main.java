6d6756076426b39535f319d790e7776e0dff3518
Connor <kor.connor@gmail.com>
2021-10-04 15:37:20 +0900

    문자열 내림차순으로 배치하기

--- src/programmers/lv1/Main.java
+++ src/programmers/lv1/Main.java
@@ -2,9 +2,10 @@ package programmers.lv1;
 
 public class Main {
 	public static void main(String[] args) {
-		문자열을정수로바꾸기withoutParseInt s = new 문자열을정수로바꾸기withoutParseInt();
-		String str = "234";
+		문자열내림차순으로배치하기 s = new 문자열내림차순으로배치하기();
+		String str = "Zbcdefg";
 		System.out.println(s.solution(str));
+		
 
 		
 	}
--- src/programmers/lv1/문자열내림차순으로배치하기.java
+++ src/programmers/lv1/문자열내림차순으로배치하기.java
@@ -6,8 +6,7 @@ public class 문자열내림차순으로배치하기 {
     public String solution(String s) {
         char[] arr = s.toCharArray();
         Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        return "3";
+        StringBuffer answer = new StringBuffer(new String(arr));
+        return answer.reverse().toString();
     }
-
 }
