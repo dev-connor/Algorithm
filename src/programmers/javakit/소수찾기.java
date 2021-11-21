e816169adaab40ec1fd04a9eae11552b8cff38ca
Connor <kor.connor@gmail.com>
2021-11-21 23:43:54 +0900

    소수찾기

---
+++ src/programmers/javakit/소수찾기.java
@@ -0,0 +1,47 @@
+package programmers.javakit;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class 소수찾기 {
+	StringBuilder str = new StringBuilder();
+	char[] arr;
+	HashSet<Integer> check = new HashSet<>();
+	HashSet<Integer> answer = new HashSet<>();
+
+	public int solution(String numbers) {
+		arr = numbers.toCharArray();
+
+		dfs();
+		return answer.size();
+	}
+
+	private void dfs() {
+		if (arr.length != str.length()) {
+			for (int i = 0; i < arr.length; i++) {
+				if (!check.contains(i)) {
+					str.append(arr[i]);
+					check.add(i);
+					
+					isPrime(str);
+					dfs();
+					
+					str.deleteCharAt(str.length()-1);
+					check.remove(i);
+				}
+			}
+		} 
+	}
+
+	private void isPrime(StringBuilder str) {
+		int n = Integer.parseInt(new String(str));
+		
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) return;
+		}
+		
+		if (n >= 2) answer.add(n);
+	}
+}
