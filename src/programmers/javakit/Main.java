141f0e5df5e7110522a40fc5681b4539ac3095ae
Connor <kor.connor@gmail.com>
2021-11-21 23:44:03 +0900

    소수찾기

---
+++ src/programmers/javakit/Main.java
@@ -0,0 +1,13 @@
+package programmers.javakit;
+
+public class Main {
+	public static void main(String[] args) {
+		소수찾기 s = new 소수찾기();
+		String numbers = "113";
+		
+		System.out.println(s.solution(numbers));
+		
+	}
+	
+
+}
