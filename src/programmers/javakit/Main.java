eae01e8710eee6f3dc0ad0d391d5880b10156045
Connor <kor.connor@gmail.com>
2022-02-16 21:25:08 +0900

    가장 큰 수

--- src/programmers/javakit/Main.java
+++ src/programmers/javakit/Main.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		주식가격 s = new 주식가격();
+		가장_큰_수 s = new 가장_큰_수();
 		
-		int[] arr = {1, 2, 3, 2, 3};
-		int[] arr2 = {4, 3, 1, 1, 0};
+		int[] arr = {3, 30, 34, 5, 9};
 		
 		
-		System.out.println(Arrays.toString(s.solution(arr)));
+		System.out.println(s.solution(arr));
 	}
 	
 
