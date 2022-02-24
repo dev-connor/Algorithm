49384c31dcb85576404b32f62728baa565eff401
Connor <kor.connor@gmail.com>
2022-02-24 22:27:43 +0900

    다리를 지나는 트럭

--- src/programmers/javakit/Main.java
+++ src/programmers/javakit/Main.java
@@ -15,11 +15,11 @@ import java.util.stream.Stream;
 
 public class Main {
 	public static void main(String[] args) {
-		더_맵게 s = new 더_맵게();
-		int[] arr = {1, 2, 3, 9, 10, 12};
+		다리를_지나는_트럭 s = new 다리를_지나는_트럭();
+		int[] arr = {10,10,10,10,10,10,10,10,10,10};
 		String[][] arr2 = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
 		
-		System.out.println(s.solution(arr, 7));
+		System.out.println(s.solution(100, 100, arr));
 		
 	}
 }
