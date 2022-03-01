028ba660d3e2748d24fca45ae27d87652ca35816
Connor <kor.connor@gmail.com>
2022-03-01 19:51:49 +0900

    주식가격 수정

--- src/programmers/javakit/주식가격.java
+++ src/programmers/javakit/주식가격.java
@@ -3,21 +3,17 @@ package programmers.javakit;
 public class 주식가격 {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        
-        for (int i = 0; i < prices.length - 1; i++) {
-        	int price = prices[i];
-        	
-        	for (int j = i + 1; j < prices.length; j++) {
-        		int nextPrice = prices[j];
-        		if (price > nextPrice) {
-        			answer[i] = j - i;
-        			break;
-        		}
-        		
-			}
-        	if (answer[i] == 0)
-        		answer[i] = prices.length - 1 - i;
-		}
+
+        for (int i = 0; i < answer.length - 1; i++) {
+            int sec = 0;
+
+            for (int j = i + 1; j < answer.length; j++) {
+                sec++;
+
+                if (prices[i] > prices[j]) break;
+            }
+            answer[i] = sec;
+        }
         return answer;
     }
 }
