eef50fc47b08b816ad583c99307232a5240919ea
Hyunjun <kor.connor@gmail.com>
2023-11-21 12:33:30 +0900

    [Silver I] Title: 정수 삼각형, Time: 296 ms, Memory: 46244 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,13 @@
+n = int(input())
+dp = [[0] * (3*n+2) for _ in range(2*n)]
+
+for i in range(n):
+    inputs = list(map(int, input().split()))
+    for j,v in enumerate(inputs):
+        dp[i][j+1] = v
+
+for i in range(1,n):
+    for j in range(1,n+1):
+        dp[i][j] = dp[i][j] + max(dp[i-1][j-1],dp[i-1][j])
+
+print(max(dp[n-1][1:n+1]))
