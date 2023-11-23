074f17caff59242b87a625cba5961b347dd9a159
Hyunjun <kor.connor@gmail.com>
2023-11-23 18:51:26 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,20 @@
 n,m = map(int, input().split())
 visited = [False] * (n+1)
+nums = []
 
-def dfs(arr, visited):
-    cnt = len(arr)
+def dfs(visited):
+    cnt = len(nums)
 
     if cnt == m:
-        print(' '.join(map(str, arr)))
+        print(' '.join(map(str, nums)))
         return
 
     for i in range(1,n+1):
         if not visited[i]:
-            arr.append(i)
+            nums.append(i)
             visited[i] = True
-            dfs(arr, visited)
-            arr.pop()
+            dfs(visited)
+            nums.pop()
             visited[i] = False
 
-dfs([], visited)
+dfs(visited)
