dc94f7fc32cd514c0691d684a0e17fa2dbe2337b
Hyunjun <kor.connor@gmail.com>
2023-11-23 16:35:18 +0900

    [Gold IV] Title: 연구소, Time: 2040 ms, Memory: 33676 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -19,16 +19,7 @@ for i in range(n):
 
 picked = list(itertools.combinations(empty, 3))
 
-def spread(pos):
-    x = pos[0]
-    y = pos[1]
-
-    n, m = map(int, input().split())
-    graph = [list(map(int, input().split())) for _ in range(n)]
-
 def spread(x,y):
-    # x = pos[0]
-    # y = pos[1]
 
     # 벗어나면 종료
     if x<0 or y<0 or x>=n or y>=m:
@@ -43,24 +34,13 @@ def spread(x,y):
         return True
     return False
 
-def count(value):
-    cnt = 0
-    for i in range(len(graph)):
-        for j in range(len(graph[0])):
-            if graph[i][j] == value:
-                cnt += 1
-    return cnt
-
-# result = 0
-# for i in range(n):
-#     for j in range(m):
-#         if dfs(i, j) == True:
-#             result += 1
-
-    # print(result)
-
-# def dfs(graph, v, visited):
-# visited[v] = True
+def count(target):
+    count = 0
+    for array in graph:
+        for value in array:
+            if value == target:
+                count += 1
+    return count
 
 max_v = 0
 for w1,w2,w3 in picked:
@@ -74,9 +54,5 @@ for w1,w2,w3 in picked:
         spread(v[0],v[1])
 
     max_v = max(max_v, count(0))
-
-    graph[w1[0]][w1[1]] = 0
-    graph[w2[0]][w2[1]] = 0
-    graph[w3[0]][w3[1]] = 0
 
 print(max_v)
