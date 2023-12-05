c0755d47d49165889bebaa1577b9ad4d58dc7ecf
Hyunjun <kor.connor@gmail.com>
2023-12-05 17:28:45 +0900

    [Silver II] Title: N과 M (9), Time: 152 ms, Memory: 34708 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,25 @@
+n,m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+answer = []
+s = set()
+visited = [False] * n
+
+def dfs():
+    cnt = len(answer)
+
+    if cnt == m:
+        if str(answer) not in s:
+            print(' '.join(map(str, answer)))
+        s.add(str(answer))
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            answer.append(nums[i])
+            visited[i] = True
+            dfs()
+            answer.pop()
+            visited[i] = False
+
+dfs()
