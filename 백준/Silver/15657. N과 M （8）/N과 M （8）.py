d67c66b8f1b37fda79cd59bee5cb83e0e95168a4
Hyunjun <kor.connor@gmail.com>
2023-12-05 17:05:53 +0900

    [Silver III] Title: N과 M (8), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,18 @@
+n,m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+answer = []
+
+def dfs(now):
+    cnt = len(answer)
+
+    if cnt == m:
+        print(' '.join(map(str, answer)))
+        return
+
+    for i in range(now, n):
+        answer.append(nums[i])
+        dfs(i)
+        answer.pop()
+
+dfs(0)
