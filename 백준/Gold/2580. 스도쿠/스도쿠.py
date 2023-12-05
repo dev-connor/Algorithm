d4edf594720ddc6c7d8548d0072ea57daf5b0268
Connor <kor.connor@gmail.com>
2023-12-05 18:50:11 +0900

    스도쿠

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,8 +1,9 @@
+# 47:46
 board = [[0] * 9 for _ in range(9)]
 zeros = []
-hodi = dict()
-vedi = dict()
-bxdi = dict()
+h_nums = dict()
+v_nums = dict()
+bx_nums = dict()
 
 def add(d, a, b):
     s = d.setdefault(a, set())
@@ -15,65 +16,54 @@ for i in range(9):
     inputs = list(map(int, input().split()))
     for j,v in enumerate(inputs):
         board[i][j] = v
-        add(hodi,i,v)
-        add(vedi,j,v)
+        add(h_nums, i, v)
+        add(v_nums, j, v)
         nx = str(i//3)
         ny = str(j//3)
-        add(bxdi,nx+ny,v)
+        add(bx_nums, nx + ny, v)
         if v == 0:
             zeros.append((i,j))
 
-# visited = [False] * (n+1)
-# nums = []
+def check_ho(x,v):
+    if v in h_nums[x]:
+        return False
+    return True
+
+def check_ve(y,v):
+    if v in v_nums[y]:
+        return False
+    return True
+
+def check_box(nx,ny,v):
+    if v in bx_nums[nx + ny]:
+        return False
+    return True
 
 def dfs(idx):
     if idx == len(zeros):
-        for i in range(9):
-            print(' '.join(map(str, board[i])))
+        for v in range(9):
+            print(' '.join(map(str, board[v])))
         exit()
-
-    def check_ho(x,y,v):
-        if v in hodi[x]:
-            return False
-        return True
-
-    def check_ve(x,y,v):
-        if v in vedi[y]:
-            return False
-        return True
-
-    def check_box(x,y,v):
-        nx = str(x//3)
-        ny = str(y//3)
-
-        if v in bxdi[nx+ny]:
-            return False
-        return True
 
     zero = zeros[idx]
     x = zero[0]
     y = zero[1]
 
-    for i in range(1,10):
-        if check_ho(x,y,i) and check_ve(x,y,i) and check_box(x,y,i):
-            # nums.append(i)
-            board[x][y] = i
+    for v in range(1,10): # 1~9 까지 넣어보기
+        nx = str(x // 3) # 박스의 위치
+        ny = str(y // 3)
 
-            add(hodi, x, i)
-            add(vedi, y, i)
-            nx = str(x // 3)
-            ny = str(y // 3)
-            add(bxdi, nx + ny, i)
+        if check_ho(x,v) and check_ve(y,v) and check_box(nx,ny,v):
+            board[x][y] = v
+            add(h_nums, x, v)
+            add(v_nums, y, v)
+            add(bx_nums, nx + ny, v)
 
-            dfs(idx+1)
+            dfs(idx+1) # 다음 빈칸 채우러가기
+
             board[x][y] = 0
-
-            remove(hodi, x, i)
-            remove(vedi, y, i)
-            nx = str(x // 3)
-            ny = str(y // 3)
-            remove(bxdi, nx + ny, i)
-
-            # nums.pop()
+            remove(h_nums, x, v)
+            remove(v_nums, y, v)
+            remove(bx_nums, nx + ny, v)
 
 dfs(0)
