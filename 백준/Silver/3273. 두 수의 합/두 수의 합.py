10b7b0125e363584536bfa9c0dd280959f42bd1c
Hyunjun <kor.connor@gmail.com>
2023-12-06 16:33:03 +0900

    [Silver III] Title: 두 수의 합, Time: 92 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,19 @@
+n = int(input())
+nums = list(map(int, input().split()))
+x = int(input())
+
+nums.sort()
+answer = 0
+f = 0
+e = n-1
+
+while f < e:
+    sum_v = nums[f] + nums[e]
+    if sum_v == x:
+        answer += 1
+    if sum_v <= x:
+        f += 1
+    else:
+        e -= 1
+
+print(answer)
