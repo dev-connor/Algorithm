2fcbda945aa5b6caf1bf2ee3c2698a308e50e396
Connor <kor.connor@gmail.com>
2023-12-06 15:30:04 +0900

    기타 알고리즘

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,16 +1,13 @@
+# 23:37
 n = int(input())
 nums = list(map(int, input().split()))
-# plus, minus, multiple, division = map(int, input().split())
-e_opers = list(map(int, input().split()))
+max_opers = list(map(int, input().split()))
 
 max_v = -int(1e9)
 min_v = int(1e9)
+opers = ['+', '-', '*', '/']
 
-oper_arr = ['+','-','*','/']
-
-# nums = []
-
-def dfs(opers, num, idx):
+def dfs(now_opers, num, idx):
     global min_v, max_v
 
     if idx == n:
@@ -19,10 +16,10 @@ def dfs(opers, num, idx):
         return
 
     for i in range(4):
-        if opers[i] < e_opers[i]:
-            opers[i] += 1
-            dfs(opers, int(eval(str(num) + oper_arr[i] + str(nums[idx]))), idx+1)
-            opers[i] -= 1
+        if now_opers[i] < max_opers[i]:
+            now_opers[i] += 1
+            dfs(now_opers, int(eval(str(num) + opers[i] + str(nums[idx]))), idx + 1)
+            now_opers[i] -= 1
 
 dfs([0,0,0,0], nums[0], 1)
 print(max_v)
