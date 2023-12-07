b0dc8dfe04fb0a0658288a026277bf433d9db152
Hyunjun <kor.pacman@gmail.com>
2023-12-07 12:10:54 +0900

    [Gold V] Title: 집합의 표현, Time: 312 ms, Memory: 77400 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,34 @@
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+parent = [i for i in range(n+1)]
+sys.setrecursionlimit(100000000)
+
+for _ in range(m):
+    oper,a,b = map(int, input().split())
+
+    if oper == 0:
+        if a == b:
+            continue
+
+        union_parent(parent,a,b)
+    else:
+        if find_parent(parent,a) == find_parent(parent,b):
+            print('YES')
+        else:
+            print('NO')
