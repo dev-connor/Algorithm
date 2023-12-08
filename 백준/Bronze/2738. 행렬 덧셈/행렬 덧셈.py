fb71c0ab16ee749cdba67832c45be2efbc09bb83
Hyunjun <kor.pacman@gmail.com>
2023-12-08 18:14:13 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+
+matrix1 = []
+for i in range(n):
+    matrix1.append(list(map(int, input().split())))
+matrix2 = []
+for i in range(n):
+    matrix2.append(list(map(int, input().split())))
+
+for i in range(n):
+    for j in range(m):
+        print(matrix1[i][j] + matrix2[i][j], end=' ')
+
+    print()
