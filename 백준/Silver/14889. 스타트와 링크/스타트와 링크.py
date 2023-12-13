82091e8f3cf29a172445402b51b40e075fd6fe48
Hyunjun <kor.pacman@gmail.com>
2023-12-13 19:28:03 +0900

    [Silver I] Title: 스타트와 링크, Time: 4476 ms, Memory: 56256 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,30 @@
+import itertools
+
+n = int(input())
+stats = []
+for i in range(n):
+    stats.append(list(map(int, input().split())))
+
+nums = []
+
+all_m = {i for i in range(n)}
+picked = list(itertools.combinations(all_m,n//2))
+min_v = int(1e9)
+
+for p in picked:
+    start = set(p)
+    link = all_m - start
+    s_sum = 0
+    l_sum = 0
+
+    s_two = list(itertools.combinations(start,2))
+    for s in s_two:
+        s_sum += stats[s[0]][s[1]] + stats[s[1]][s[0]]
+
+    l_two = list(itertools.combinations(link,2))
+    for l in l_two:
+        l_sum += stats[l[0]][l[1]] + stats[l[1]][l[0]]
+
+    min_v = min(min_v, abs(s_sum - l_sum))
+
+print(min_v)
