b09a83b33aad984abbc8a3f402c079738c9c0130
Hyunjun <kor.pacman@gmail.com>
2023-12-20 00:33:36 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1560 ms, Memory: 116424 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,11 @@
+import bisect
+
+n = int(input())
+cards = list(map(int, input().split()))
+m = int(input())
+nums = list(map(int, input().split()))
+
+cards.sort()
+
+for n in nums:
+    print(bisect.bisect_right(cards,n) - bisect.bisect_left(cards,n))
