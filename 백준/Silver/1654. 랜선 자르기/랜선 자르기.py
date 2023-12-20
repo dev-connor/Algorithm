efabf475aace40cff435b13f4c737f016397e3da
Hyunjun <kor.pacman@gmail.com>
2023-12-20 23:51:30 +0900

    [Silver II] Title: 랜선 자르기, Time: 448 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,23 @@
+k,n = map(int, input().split())
+nums = [0] * k
+for i in range(k):
+    nums[i] = int(input())
+
+start = 1
+end = max(nums)
+
+result = 0
+while start <= end:
+    cnt = 0
+    mid = (start + end) // 2
+
+    for x in nums:
+        cnt += x//mid
+        
+    if cnt < n:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
