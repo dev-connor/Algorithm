7b53e1c9c4e354ff13c2fed0492b93ea91476ae0
Hyunjun <kor.pacman@gmail.com>
2023-12-21 01:26:39 +0900

    [Gold I] Title: K번째 수, Time: 1172 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,24 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+K = int(sys.stdin.readline().rstrip())
+
+start = 1
+end = N * N
+result = 0
+
+while start <= end:
+    cnt = 0
+
+    mid = (start + end) // 2
+
+    for div in range(1, N + 1):
+        cnt += min(mid // div, N)
+    
+    if cnt >= K:
+        result = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(result)
