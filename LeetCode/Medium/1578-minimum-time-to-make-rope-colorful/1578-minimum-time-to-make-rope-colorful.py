2593e3f3746bf0d35d5bbdace30f06606cd27fb1
Hyunjun <kor.pacman@gmail.com>
2023-12-27 15:16:09 +0900

    Time: 892 ms (68.06%), Space: 28.8 MB (6.39%) - LeetHub

--- LeetCode/Medium/1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ LeetCode/Medium/1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,30 +1,21 @@
 import heapq
 from typing import List
 
-
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
         answer = 0
         n = len(colors)
 
-        i = 0
-        while i < n-1:
-            h = []
-            if colors[i] == colors[i+1]:
-                heapq.heappush(h,neededTime[i])
+        h = []
+        for i in range(n):
+            if i+1 < n and colors[i] == colors[i+1]:
+                if not h:
+                    heapq.heappush(h,neededTime[i])
                 heapq.heappush(h,neededTime[i+1])
-                i += 1
-
-                while i+1 < n and colors[i] == colors[i+1]:
-                    heapq.heappush(h,neededTime[i+1])
-                    i += 1
-                    if i >= n-1:
-                        break
-                while len(h) > 1:
-                    answer += heapq.heappop(h)
-                i += 1
 
             else:
-                i += 1
+                while len(h) > 1:
+                    answer += heapq.heappop(h)
+                h.clear()
 
         return answer
