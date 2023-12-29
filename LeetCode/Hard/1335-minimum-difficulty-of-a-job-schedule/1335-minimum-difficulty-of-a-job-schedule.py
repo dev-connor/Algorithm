42c59e48aba0f25a5f4d21403ee82cf03da7f5c4
Hyunjun <kor.pacman@gmail.com>
2023-12-29 16:08:11 +0900

    Time: 2680 ms (5.9%), Space: 17.4 MB (63%) - LeetHub

---
+++ LeetCode/Hard/1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+
+        if n < d:
+            return -1
+
+        dp = [[1e9] * n for _ in range(d)]
+
+        for i in range(n):
+            dp[0][i] = max(jobDifficulty[:i+1])
+
+        for i in range(1,d):
+            for j in range(n):
+                for k in range(j):
+                    dp[i][j] = min(dp[i][j],dp[i-1][k]+max(jobDifficulty[k+1:j+1]))
+
+
+        return dp[-1][-1]
