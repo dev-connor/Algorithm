4bf45ec026bf3ec315bbd75043e47eeed637ad2e
Hyunjun <kor.pacman@gmail.com>
2024-01-02 18:02:48 +0900

    Time: 71 ms (7.11%), Space: 17.4 MB (5.17%) - LeetHub

---
+++ LeetCode/Medium/2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        answer = [[] for _ in range(1)]
+        for n in nums:
+            for arr in answer:
+                if n not in arr:
+                    arr.append(n)
+                    break
+            else:
+                answer.append([n])
+        return answer
