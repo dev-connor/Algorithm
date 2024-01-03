13ccc8aedb6d1359b6873e2e925b88f47c8b6682
Hyunjun <kor.pacman@gmail.com>
2024-01-03 16:53:17 +0900

    [Gold II] Title: 친구 네트워크, Time: 508 ms, Memory: 175408 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -23,10 +23,8 @@ for _ in range(int(input())):
     parent, number = {}, {}
     for i in range(num):
         a, b = input().split()
-        if a not in parent:
-            parent[a] = a
-            number[a] = 1
-        if b not in parent:
-            parent[b] =b
-            number[b] = 1
+        parent.setdefault(a,a)
+        parent.setdefault(b,b)
+        number.setdefault(a,1)
+        number.setdefault(b,1)
         union(a, b)
