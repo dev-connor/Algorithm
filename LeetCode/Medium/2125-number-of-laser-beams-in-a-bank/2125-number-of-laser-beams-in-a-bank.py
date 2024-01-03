38602e2d91eb9a0911de13d01488da32070897bc
Hyunjun <kor.pacman@gmail.com>
2024-01-03 12:48:49 +0900

    Time: 94 ms (74.03%), Space: 19.6 MB (6.86%) - LeetHub

---
+++ LeetCode/Medium/2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        answer = 0
+        counts = []
+
+        for line in bank:
+            count = line.count('1')
+            if count != 0:
+                counts.append(count)
+        
+        if len(counts) == 0:
+            return 0 
+        
+        before = counts[0]
+        for i in range(1,len(counts)):
+            answer += before*counts[i]
+            before = counts[i]
+
+        return answer
