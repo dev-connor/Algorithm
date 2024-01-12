9cc71c9d7f43dc350aad49696dd2336115f7a8db
Hyunjun <kor.pacman@gmail.com>
2024-01-12 16:53:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 392 ms, Memory: 189588 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,17 @@
-n, k = map(int, input().split())
-
-thing = [[0,0]]
-d = [[0]*(k+1) for _ in range(n+1)]
-
+n,k = map(int, input().split())
+items = [0]
 for i in range(n):
-    thing.append(list(map(int, input().split())))
+    items.append(list(map(int, input().split())))
+dp = [[0] * (k+1) for _ in range(n+1)]
 
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        w = thing[i][0]
-        v = thing[i][1]
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        weight = items[i][0]
+        value = items[i][1]
 
-        if j < w:
-            d[i][j] = d[i-1][j]
+        if weight <= j: # 들 수 있는 무게일 때
+            dp[i][j] = max(dp[i-1][j-weight]+value,dp[i-1][j])
         else:
-            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
+            dp[i][j] = dp[i-1][j]
 
-print(d[n][k])
+print(dp[-1][-1])
