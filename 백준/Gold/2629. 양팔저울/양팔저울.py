5c9f7d0701cc14596261e2ac53d8c9cfb06ae379
Hyunjun <kor.pacman@gmail.com>
2024-01-12 17:39:49 +0900

    [Gold III] Title: 양팔저울, Time: 160 ms, Memory: 130060 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -1,8 +1,8 @@
 n = int(input())
 weights = [0]+list(map(int, input().split()))
 m = int(input())
-beads = [0]+list(map(int, input().split()))
-k = 80000
+beads = list(map(int, input().split()))
+k = 40000
 
 dp = [[0] * (2*k+1) for _ in range(n+1)]
 for i in range(n+1):
@@ -11,19 +11,10 @@ for i in range(n+1):
 for i in range(1,n+1):
     for j in range(1,2*k+1):
         weight = weights[i]
-        # bead = beads[i]
-
         dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight],dp[i-1][(j+weight)%(2*k+1)])
 
-        # if weight <= j: # 들 수 있는 무게일 때
-        #     dp[i][j] = max(dp[i-1][j-weight]+value,dp[i-1][j])
-        # else:
-        #     dp[i][j] = dp[i-1][j]
-
-for bead in beads[1:]:
+for bead in beads:
     if dp[-1][bead] == 1:
         print('Y')
     else:
         print('N')
-
-# print(dp[-1][-1])
