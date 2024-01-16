8e8f008d864871984a9f83b33c6fc966fba92f02
Hyunjun <kor.pacman@gmail.com>
2024-01-16 19:51:50 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 772 ms, Memory: 113512 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,19 +1,19 @@
 n = int(input())
-matrix = [[0,0]]
+arr = [[0, 0]]
 for i in range(n):
-    matrix.append(list(map(int, input().split())))
+    arr.append(list(map(int, input().split())))
 
 INF = int(1e9)
 dp = [[INF] * (n+1) for _ in range(n+1)]
-for i in range(1,n):
-    dp[i][i+1] = matrix[i][0] * matrix[i][1] * matrix[i+1][1]
-for i in range(1,n+1):
+
+for i in range(1,n+1): 
     dp[i][i] = 0
 
-for t in range(2,n):
+for t in range(1,n):
     for i in range(1,n-t+1):
-        for mid in range(i,n):
-            value = matrix[i][0]*matrix[mid][1]*matrix[i+t][1]
-            dp[i][i+t] = min(dp[i][i+t], dp[i][mid]+dp[mid+1][i+t]+value)
+        end = i+t
+        for mid in range(i,end):
+            value = arr[i][0] * arr[mid][1] * arr[end][1]
+            dp[i][end] = min(dp[i][end], dp[i][mid]+dp[mid+1][end]+value)
 
 print(dp[1][n])
