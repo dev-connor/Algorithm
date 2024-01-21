36a2ea4eaa4ba99382387777e1c1cf9940b8251f
Hyunjun <kor.pacman@gmail.com>
2024-01-21 22:40:17 +0900

    Time: 36 ms (71.38%), Space: 16.6 MB (56.96%) - LeetHub

---
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1: return nums[0]
+        n = len(nums)
+        dp = [0]*n
+        dp[0],dp[1] = nums[0],max(nums[:2])
+
+        for i in range(2,n):
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+
+        return max(dp)
