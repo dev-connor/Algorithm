03e7d37b1354743881836d1d64bb31621a1dfcf3
Hyunjun <kor.pacman@gmail.com>
2024-02-11 16:37:52 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 284 ms, Memory: 117056 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,21 +1,26 @@
 import math
 
-max_ = int(1e6)
-nums = [True for i in range(max_ + 1)] # 확인할 소수의 최대값
-nums[0] = nums[1] = False # 0 과 1 은 소수가 아님
 
-for i in range(2, int(math.sqrt(max_)) + 1):
-    if nums[i]:
-        j = 2
-        while i * j <= max_:
-            nums[i * j] = False # 배수들은 소수가 아님
-            j += 1
+def eratosthenes(max_):
+    nums = [True for _ in range(max_ + 1)]  # 확인할 소수의 최대값
+    nums[0] = nums[1] = False  # 0 과 1 은 소수가 아님
+
+    for i in range(2, int(math.sqrt(max_)) + 1):
+        if nums[i]:
+            j = 2
+            while i * j <= max_:
+                nums[i * j] = False  # 배수들은 소수가 아님
+                j += 1
+    return nums
+
+end = int(1e6)
+nums = eratosthenes(end)
 
 t = int(input())
 for _ in range(t):
     n = int(input())
-    answer = 0
-    for i in range(1,n//2+1):
+    ans = 0
+    for i in range(2,n//2+1):
         if nums[i] and nums[n-i]:
-            answer+=1
-    print(answer)
+            ans += 1
+    print(ans)
