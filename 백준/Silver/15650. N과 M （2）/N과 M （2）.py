8df099f6dec049cb3d293fb30e9bcc7de262f9fd
Hyunjun <kor.pacman@gmail.com>
2024-03-02 21:25:23 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,15 +1,7 @@
+from itertools import combinations
+
 n,cnt = map(int,input().split())
-nums = []
 
-def dfs(now):
-    if len(nums) == cnt:
-        ans = ' '.join(map(str, nums))
-        print(ans)
-        return
-    
-    for i in range(now, n+1):
-        nums.append(i)
-        dfs(i+1)
-        nums.pop()
-
-dfs(1)
+for nums in combinations([i for i in range(1,n+1)],cnt):
+    ans = ' '.join(map(str, nums))
+    print(ans)
