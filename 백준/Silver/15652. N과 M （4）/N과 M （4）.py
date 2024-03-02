248fe6bf892f295f70d58157320f830d2a02dc95
Hyunjun <kor.pacman@gmail.com>
2024-03-02 21:10:28 +0900

    [Silver III] Title: N과 M (4), Time: 160 ms, Memory: 112464 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,16 +1,16 @@
-n,m = map(int, input().split())
+n,cnt = map(int,input().split())
 nums = []
 
 def dfs(now):
-    cnt = len(nums)
-
-    if cnt == m:
-        print(' '.join(map(str, nums)))
+    if len(nums) == cnt:
+        ans = ' '.join(map(str, nums))
+        print(ans)
         return
-
-    for i in range(now, n + 1):
+    
+    for i in range(now, n+1):
         nums.append(i)
         dfs(i)
         nums.pop()
-
+        
 dfs(1)
+    
